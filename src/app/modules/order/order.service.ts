8ce6093192a47a0a5cab3ffc6e675a5885f30767
Md. Ruhul Amin <dev.ruhulamin3@gmail.com>
2023-06-29 16:00:04 +0600

    get single order route some changes

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -92,8 +92,7 @@ export const getSingleOrderService = async (
   id: string
 ): Promise<IOrder | null> => {
   const order = await Order.findById(id).populate("cow").populate("buyer");
-  console.log(order);
-  if (order && order.buyer._id != user.id) {
+  if (order && order.buyer.id != user.id) {
     throw new ApiError(
       httpStatus.UNAUTHORIZED,
       "you are not owner of the order"
