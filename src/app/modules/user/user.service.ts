0cf88bae50489b548869607766dac49af8ea4667
RuhulAmin3 <dev.ruhulamin3@gmail.com>
2023-07-07 16:34:33 +0600

    cow hut backend done

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import bcrypt from "bcrypt";
 import { SortOrder } from "mongoose";
 import { calculatePagination } from "../../../shared/caculatePagination";
 import { IUserFilterData, IUser, IProfile } from "./user.interface";
